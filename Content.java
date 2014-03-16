52ae9d5a17faba2f9b5bf8365794c9af32ff99e1
Rohan D. Shah <rohan.shah3@aggiemail.usu.edu>
2014-03-16 02:13:58 -0600

    Enumeration that stores the symbols of the cells
    This is a class which stores a list of constants. These constants are
    used to display the contents of the cell and the player's symbol.

---
+++ Content.java
@@ -0,0 +1,16 @@
+/**
+ * Description:
+ * This is a list of constants that stores various values of the cells in the grid as well as identifies the player.
+ * CROSS = Determines the "X" symbol in the cell and identifies the player who is having this symbol.
+ * NOUGHT = Determines the "O" symbol in the cell and identifies the player who is having this symbol.
+ * EMPTY = Determines an empty space in the cell.
+ */
+
+/**
+ *
+ * @author Rohan D. Shah
+ */
+public enum Content 
+{
+    CROSS, NOUGHT, EMPTY;
+}
